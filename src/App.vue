1df5ea205bad364a059a403de9ac7ba304471c2c
Johannes Brandenburger <79154528+johannesbrandenburger@users.noreply.github.com>
2023-11-29 16:29:55 +0100

    better zoom setting

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,19 @@
 <template>
   <div style="height: 100vh; width: 100vw">
     <l-map
-      v-model="zoom"
-      v-model:zoom="zoom"
       :center="[47.41322, -1.219482]"
-      @move="log('move')"
       @click="onClickOnMap"
+      :min-zoom="1"
+      :max-zoom="10"
+      :zoom-animation="true"
     >
       <l-tile-layer
-        url="https://tiles.stadiamaps.com/tiles/stamen_watercolor/{z}/{x}/{y}.jpg"
+        url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
         min-zoom="1"
         max-zoom="14"
-        attribution="&copy; <a href=&quot;https://stadiamaps.com/&quot;>Stadia Maps</a>, &copy; <a href=&quot;https://openmaptiles.org/&quot;>OpenMapTiles</a> &copy; <a href=&quot;http://openstreetmap.org&quot;>OpenStreetMap</a> contributors"
+        inertia-max-speed="500"
+        attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
       />
-      <l-tile-layer
-        url="https://tiles.stadiamaps.com/tiles/stamen_terrain_labels/{z}/{x}/{y}{r}.png"
-        :options="tileLayerOptions"
-        attribution="&copy; <a href=&quot;https://stadiamaps.com/&quot;>Stadia Maps</a>, &copy; <a href=&quot;https://openmaptiles.org/&quot;>OpenMapTiles</a> &copy; <a href=&quot;http://openstreetmap.org&quot;>OpenStreetMap</a> contributors"
-      />
-      <l-control-layers />"
-      <!-- do a polygon for all polygon in the array -->
       <l-polygon
         v-for="(polygon, index) in polygons"
         :key="index"
@@ -41,7 +35,7 @@ import {
   LPolygon
 } from "@vue-leaflet/vue-leaflet";
 import "leaflet/dist/leaflet.css";
-// @ts-ignore
+
 import {
   getPlaceByNominatimData,
   getCoordsFromSearchText,
@@ -49,61 +43,45 @@ import {
   getObjectFromLocalStorage,
   formatAddressObject,
   findHumanPlaceName,
-getOutlineForLatLng,
+  getOutlineForLatLng
 } from "./js/helpers.js";
 
 export default {
   components: {
     LMap,
     LTileLayer,
-    LControlLayers,
     LPolygon
   },
   data() {
     return {
-      zoom: 2,
-      iconWidth: 25,
-      iconHeight: 40,
-      polygons: [{
-        key: 1,
-        latlngs: [
-          [47.41322, -1.219482],
-          [47.41322, -1.219482],
-        ],
-        color: 'red',
-      }],
+      /** @type {Array<{key: number, latlngs: number[][], color: string}>} */
+      polygons: [],
       tileLayerOptions: {
         maxZoom: 18,
-        minZoom: 1,
-      },
+        minZoom: 1
+      }
     };
   },
-  computed: {
-  },
+  computed: {},
   methods: {
-    log(a) {
-      console.log(a);
-    },
-    changeIcon() {
-      this.iconWidth += 2;
-      if (this.iconWidth > this.iconHeight) {
-        this.iconWidth = Math.floor(this.iconHeight / 2);
-      }
-    },
+    /**
+     * Triggered when the user clicks on the map
+     * @param {L.LeafletMouseEvent} event
+     */
     async onClickOnMap(event) {
       const { lat, lng } = event.latlng || {};
       if (isNaN(lat) || isNaN(lng)) return;
 
       console.log(`click event on map at lat: ${lat}, lng: ${lng}`);
 
+      // fetch the outline for the clicked region
       const polygonLatlngs = await getOutlineForLatLng(lat, lng);
       this.polygons.push({
         key: this.polygons.length + 1,
         latlngs: polygonLatlngs,
-        color: 'red',
+        color: "red"
       });
-      
-    },
-  },
+    }
+  }
 };
 </script>
