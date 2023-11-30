3fe876bd042f55b50ab92e54402b8567abb392d1
Johannes Brandenburger <79154528+johannesbrandenburger@users.noreply.github.com>
2023-11-30 01:33:50 +0100

    markers

--- simple-server.js
+++ simple-server.js
@@ -1,11 +1,13 @@
 // imports
 const express = require("express");
 const cors = require("cors");
+const bodyParser = require("body-parser");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // setup express server
 const app = express();
 app.use(cors());
+app.use(bodyParser.json());
 const port = 8081;
 
 // setup mongo client
@@ -19,9 +21,11 @@ async function main() {
     const userCollection = database.collection("users");
     const pinCollection = database.collection("pins");
     
-    // create a document to insert
+    // print out the current users and pins
     const users = await userCollection.find({}).toArray();
     console.log(users);
+    const pins = await pinCollection.find({}).toArray();
+    console.log(pins);
 
     // configure different endpoints
     app.get("/user", async (req, res) => {
@@ -31,17 +35,27 @@ async function main() {
 
     app.post("/user", async (req, res) => {
         const user = req.body;
+
+        // if user already exists, return the existing user (by name)
+        const existingUser = await userCollection.findOne({name: user.name});
+        if (existingUser) {
+            res.send(existingUser);
+            return;
+        }
+
+        // otherwise, create a new user
         await userCollection.insertOne(user);
+
         res.send(user);
     });
 
-    app.get("/user/:id", async (req, res) => {
-        const user = await userCollection.findOne({id: req.params.id});
+    app.get("/user/:name", async (req, res) => {
+        const user = await userCollection.findOne({name: req.params.name});
         res.send(user);
     });
 
-    app.get("/pin/:user_id", async (req, res) => {
-        const pins = await pinCollection.find({user_id: req.params.user_id}).toArray();
+    app.get("/pin/:username", async (req, res) => {
+        const pins = await pinCollection.find({username: req.params.username}).toArray();
         res.send(pins);
     });
 
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,23 @@
 <template>
   <input
-    v-if="!isLogged"
+    v-if="!isLoggedIn"
     type="text"
     placeholder="Login"
     v-model="login"
     @keyup.enter="loginUser"
   >
-  <div v-if="isLogged">
+  <div v-if="isLoggedIn">
     <div>Logged in as {{ login }}</div>
     <button @click="logoutUser">
       Logout
     </button>
   </div>
-  <div style="height: 100vh; width: 100vw">
+  <DataInputDialog
+    v-if="clickedOnMap"
+    @save="saveData"
+    @cancel="cancelData"
+  />
+  <div style="height: 90vh; width: 90vw;">
     <l-map
       :center="[47.41322, -1.219482]"
       :zoom="5"
@@ -20,6 +25,7 @@
       :min-zoom="1"
       :max-zoom="10"
       :zoom-animation="true"
+      style="cursor: crosshair;"
     >
       <l-tile-layer
         url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
@@ -27,6 +33,15 @@
         max-zoom="14"
         inertia-max-speed="500"
         attribution="&copy; <a href=&quot;https://stadiamaps.com/&quot;>Stadia Maps</a>, &copy; <a href=&quot;https://openmaptiles.org/&quot;>OpenMapTiles</a> &copy; <a href=&quot;http://openstreetmap.org&quot;>OpenStreetMap</a> contributors"
+      />
+      <l-polygon
+        v-for="marker in markers"
+        :key="marker.key"
+        :lat-lngs="[marker.latlng]"
+        :color="'green'"
+        :weight="5"
+        :z-index-offset="100000"
+        :fill="true"
       />
       <l-polygon
         v-for="polygon in polygons"
@@ -46,37 +61,35 @@ import {
   LMap,
   LTileLayer,
   LControlLayers,
+  LRectangle,
   LPolygon
 } from "@vue-leaflet/vue-leaflet";
 import "leaflet/dist/leaflet.css";
-
-import {
-  getPlaceByNominatimData,
-  getCoordsFromSearchText,
-  saveObjectToLocalStorage,
-  getObjectFromLocalStorage,
-  formatAddressObject,
-  findHumanPlaceName,
-  getOutlineForLatLng,
-getRandomPastelColor
-} from "./js/helpers.js";
+import DataInputDialog from "./components/DataInputDialog.vue";
+import { getOutlineForLatLng, getRandomPastelColor } from "./js/helpers.js";
+import * as requests from "./js/requests.js";
 
 export default {
   components: {
     LMap,
     LTileLayer,
-    LPolygon
+    LPolygon,
+    DataInputDialog
   },
   data() {
     return {
       /** @type {Array<{key: number, latlngs: number[][], color: string}>} */
       polygons: [],
+      /** @type {Array<{key: number, latlng: number[]}>} */
+      markers: [],
       tileLayerOptions: {
         maxZoom: 18,
         minZoom: 1
       },
-      isLogged: false,
-      login: ""
+      isLoggedIn: false,
+      login: "",
+      clickedOnMap: false,
+      selectedCoords: { lat: 0, lng: 0 }
     };
   },
   computed: {},
@@ -86,42 +99,120 @@ export default {
      * @param {L.LeafletMouseEvent} event
      */
     async onClickOnMap(event) {
+
       const { lat, lng } = event.latlng || {};
       if (isNaN(lat) || isNaN(lng)) return;
 
       console.log(`click event on map at lat: ${lat}, lng: ${lng}`);
-
-      // fetch the outline for the clicked region
-      const polygonLatlngs = await getOutlineForLatLng(lat, lng);
-      this.polygons.push({
-        key: this.polygons.length + 1,
-        latlngs: polygonLatlngs,
-        color: getRandomPastelColor()
-      });
+      
+      this.clickedOnMap = true;
+      this.selectedCoords = { lat, lng };
     },
     /**
      * Triggered when the user clicks on the login input
      */
-    loginUser() {
+    async loginUser() {
       if (this.login.length > 0) {
-        this.isLogged = true;
+        requests.createUser({ name: this.login , age: 0, homeLocation: "" });
+        this.isLoggedIn = true;
         localStorage.setItem("login", this.login);
+
+        this.markers = [];
+        this.polygons = [];
+
+        await this.updatePinsAndPolygons();
       }
     },
     /**
      * Triggered when the user clicks on the logout button
      */
     logoutUser() {
-      this.isLogged = false;
+      this.isLoggedIn = false;
       this.login = "";
       localStorage.removeItem("login");
+    },
+    /**
+     * Saves the data from the DataInputDialog
+     * @param {Object} data
+     * @param {string} data.name
+     * @param {string} data.description
+     * @param {string} data.date
+     * @param {string} data.companions
+     * @param {string} data.duration
+     * @param {string} data.budget
+     */
+    async saveData(data) {
+      console.log("saveData", data);
+
+      const { lat, lng } = this.selectedCoords;
+      const saveResponse = await requests.createPin({
+        username: this.login,
+        name: data.name,
+        description: data.description,
+        date: data.date,
+        companions: data.companions,
+        duration: data.duration,
+        budget: data.budget,
+        latitude: lat,
+        longitude: lng
+      });
+
+      // add the marker
+      this.markers.push({
+        key: this.markers.length + 1,
+        latlng: [lat, lng],
+      });
+
+      // add also the polygon
+      const polygonLatlngs = await getOutlineForLatLng(lat, lng);
+      this.polygons.push({
+        key: this.polygons.length + 1,
+        latlngs: polygonLatlngs,
+        color: getRandomPastelColor()
+      });
+      this.clickedOnMap = false;
+    },
+    /**
+     * Cancels the DataInputDialog
+     */
+    cancelData() {
+      this.clickedOnMap = false;
+    },
+    async updatePinsAndPolygons() {
+      this.markers = [];
+      this.polygons = [];
+
+      // fetch the pins for the logged in user and add them to the map
+      const pins = await requests.getPinsOfUser(this.login);
+      pins.forEach((pin) => {
+        this.markers.push({
+          key: this.markers.length + 1,
+          latlng: [pin.latitude, pin.longitude],
+        });
+      });
+
+      // fetch the outline for the pins
+      pins.forEach(async (pin) => {
+        const polygonLatlngs = await getOutlineForLatLng(pin.latitude, pin.longitude);
+        this.polygons.push({
+          key: this.polygons.length + 1,
+          latlngs: polygonLatlngs,
+          color: getRandomPastelColor()
+        });
+      });
     }
+
   },
-  mounted() {
+  async mounted() {
     const login = localStorage.getItem("login");
+    console.log("login", login);
     if (login) {
+      const user = await requests.getUser(login);
+      console.log("user", user);
       this.login = login;
-      this.isLogged = true;
+      this.isLoggedIn = true;
+
+      await this.updatePinsAndPolygons();
     }
   }
 };
