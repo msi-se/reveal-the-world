db7cd3b2e5bda584bb46dfcddfcd0f08e5a6f69d
Johannes Brandenburger <79154528+johannesbrandenburger@users.noreply.github.com>
2023-12-27 10:44:37 +0100

    create user service and tests for pin service

--- services/user-service/index.js
+++ services/user-service/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
 
 // use .env file in parent directory (only needed for local development)
 import dotenv from "dotenv";
@@ -7,6 +8,7 @@ import path from "path";
 import { fileURLToPath } from "url";
 dotenv.config({ path: path.join(path.dirname(fileURLToPath(import.meta.url)), "../.env") });
 const MONDODB_URI = process.env.MONGODB_URI || "mongodb://localhost:27017";
+const JWT_SECRET = process.env.JWT_SECRET || "TEST_SECRET";
 
 // connect to MongoDB
 const db = mongoose.connection;
@@ -17,10 +19,68 @@ await mongoose.connect(MONDODB_URI);
 // start express server
 const app = express();
 const port = 3001;
+app.use(express.json());
+
+// define user schema and model
+const userSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+const User = mongoose.model("User", userSchema);
 
 // define routes
 app.get("/", (req, res) => {
-    res.json({ message: "Hello from user-service" });
+    res.send("User service is running");
+});
+
+app.post("/login", async (req, res) => {
+
+    // get username and password from request body
+    const { username, password } = req.body;
+    if (!username || !password) {
+        res.status(400).send("Username or password missing");
+        return;
+    }
+
+    // check if user exists
+    const existingUser = await User.find({ username: username });
+    if (existingUser.length === 0) {
+        // create new user
+        const newUser = new User({ username: username, password: password });
+        await newUser.save();
+    } else {
+        // check if password is correct
+        if (existingUser[0].password !== password) {
+            res.status(400).send("Wrong password");
+            return;
+        }
+    }
+
+    // create token (valid for 1 week) (later done by fusionauth?)
+    const token = jwt.sign({ username: username }, JWT_SECRET, { expiresIn: "7d" });
+    res.json({ token: token });
+});
+
+app.post("/verify", async (req, res) => {
+    
+    // get token from request body
+    const { token } = req.body;
+    if (!token) {
+        res.status(400).send("Token missing");
+        return;
+    }
+
+    // verify token
+    try {
+        const decoded = jwt.verify(token, JWT_SECRET);
+        if (typeof decoded !== "object" || !decoded.username) {
+            res.status(400).send("Invalid token");
+            return;
+        }
+        res.json({ username: decoded.username });
+    } catch (err) {
+        res.status(400).send("Invalid token");
+    }
 });
 
 app.listen(port, () => console.log(`Example app listening on http://localhost:${port}`));
--- test/services/services.test.js
+++ test/services/services.test.js
@@ -61,6 +61,61 @@ describe("user-service-test", () => {
 
     });
 
+});
 
+describe("pin-service-test", () => {
+
+    // pin: {username: string, longitude: number, latitude: number, name: string, description: string, date: string, companions: string, duration: string, budget: string}
+    const pin = {
+        username: "testuser",
+        longitude: 1.0,
+        latitude: 1.0,
+        name: "testname",
+        description: "testdescription",
+        date: "testdate",
+        companions: "testcompanions",
+        duration: "testduration",
+        budget: "testbudget"
+    };
+
+    it("check if pin service is running", async () => {
+        const response = await fetch("http://localhost/api/pin");
+        expect(response.status).toEqual(200);
+    });
+
+    it("check if a pin can be created", async () => {
+        const response = await fetch("http://localhost/api/pin", {
+            method: "POST",
+            body: JSON.stringify(pin),
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + token
+            }
+        });
+        expect(response.status).toEqual(200);
+        const responseBody = await response.json();
+        for (const key in pin) {
+            expect(responseBody[key]).toEqual(pin[key]);
+        }
+
+        // check if the polygon outline is included in the response
+        expect(responseBody.polygon).toBeDefined();
+    });
+
+    it("check if the pins of a user can be retrieved", async () => {
+        const response = await fetch("http://localhost/api/pin/user/testuser", {
+            method: "GET",
+            headers: {
+                "Content-Type": "application/json",
+                "Authorization": "Bearer " + token
+            }
+        });
+        expect(response.status).toEqual(200);
+        const responseBody = await response.json();
+        expect(responseBody.length).toEqual(1);
+        for (const key in pin) {
+            expect(responseBody[0][key]).toEqual(pin[key]);
+        }
+    });
 
 });
