81d618132c9bf2a50b9a37549c2e82c41a1e6e39
Fabian <fabian.klimpel2002@gmail.com>
2024-01-11 10:57:24 +0100

    frontend tenant config

--- frontend/src/js/tenant.js
+++ frontend/src/js/tenant.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-undef */
-// const TENANT = process.env.VUE_APP_TENANT;
-// const TENANT_CLIENT_ID = process.env.VUE_APP_TENANT_CLIENT_ID;
-const TENANT = "default";
-const TENANT_CLIENT_ID = "e9fdb985-9173-4e01-9d73-ac2d60d1dc8e";
+const TENANT = import.meta.env.VITE_TENANT;
+const TENANT_CLIENT_ID = import.meta.env.VITE_TENANT_CLIENT_ID;
+// const TENANT = "default";
+// const TENANT_CLIENT_ID = "e9fdb985-9173-4e01-9d73-ac2d60d1dc8e";
+
+console.log(TENANT);
 
 export {
     TENANT,
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import { fileURLToPath, URL } from 'node:url'
 
 import { defineConfig } from 'vite'
@@ -8,6 +9,15 @@ export default defineConfig({
   plugins: [
     vue(),
   ],
+  build: {
+    commonjsOptions: {
+      transformMixedEsModules: true
+    }
+  },
+  define: {
+    VITE_TENANT: process.env.VITE_BACKEND_URL,
+    VITE_TENANT_CLIENT_ID: process.env.VITE_BACKEND_URL,
+  },
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
