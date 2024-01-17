afeb582f23a91cec23433780b0da56ce28c62ded
Johannes Brandenburger <79154528+johannesbrandenburger@users.noreply.github.com>
2024-01-17 23:25:57 +0100

    ui redesign

--- frontend/src/views/PinView.vue
+++ frontend/src/views/PinView.vue
@@ -1,55 +1,52 @@
 <template>
-  <main>
-    <DataInputDialog v-if="clickedOnMap" @save="saveData" @cancel="cancelData" />
-    <div style="height: 90vh; width: 90vw">
-      <l-map
-        :center="[47.41322, -1.219482]"
-        :zoom="5"
-        @click="onClickOnMap"
-        :min-zoom="1"
-        :max-zoom="10"
-        :zoom-animation="true"
-        style="cursor: crosshair"
-      >
-        <l-tile-layer
-          url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
-          min-zoom="1"
-          max-zoom="14"
-          inertia-max-speed="500"
-          attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-        />
-        <l-polygon
-          v-for="marker in markers"
-          :key="marker.key"
-          :lat-lngs="[marker.latlng]"
-          :color="'green'"
-          :weight="5"
-          :z-index-offset="100000"
-          :fill="true"
-        />
-        <l-polygon
-          v-for="polygon in polygons"
-          :key="polygon.key"
-          :lat-lngs="polygon.latlngs"
-          :color="polygon.color"
-          :weight="0"
-          :fill-opacity="0.8"
-          :fill="true"
-          :fill-color="polygon.color"
-        />
-      </l-map>
-    </div>
-  </main>
+  <DataInputDialog v-if="clickedOnMap" @save="saveData" @cancel="cancelData" />
+  <div style="height: 90vh; width: 100vw">
+    <l-map
+      :center="[47.41322, -1.219482]"
+      :zoom="5"
+      @click="onClickOnMap"
+      :min-zoom="1"
+      :max-zoom="10"
+      :zoom-animation="true"
+      style="cursor: crosshair"
+    >
+      <l-tile-layer
+        url="https://tiles.stadiamaps.com/tiles/alidade_smooth/{z}/{x}/{y}{r}.png"
+        min-zoom="1"
+        max-zoom="14"
+        inertia-max-speed="500"
+        attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+      />
+      <l-polygon
+        v-for="marker in markers"
+        :key="marker.key"
+        :lat-lngs="[marker.latlng]"
+        :color="'green'"
+        :weight="5"
+        :z-index-offset="100000"
+        :fill="true"
+      />
+      <l-polygon
+        v-for="polygon in polygons"
+        :key="polygon.key"
+        :lat-lngs="polygon.latlngs"
+        :color="polygon.color"
+        :weight="0"
+        :fill-opacity="0.8"
+        :fill="true"
+        :fill-color="polygon.color"
+      />
+    </l-map>
+  </div>
 </template>
 <script>
-import { LMap, LTileLayer, LControlLayers, LRectangle, LPolygon } from "@vue-leaflet/vue-leaflet"
-import "leaflet/dist/leaflet.css"
-import DataInputDialog from "../components/DataInputDialog.vue"
-import { getOutlineForLatLng, getRandomPastelColor } from "../js/helpers.js"
-import { getPolygonAndName } from "../js/helpers-new.js"
-import * as requests from "../js/requests.js"
-import { getUser, login, logout } from '../js/user';
-
+import { LMap, LTileLayer, LControlLayers, LRectangle, LPolygon } from '@vue-leaflet/vue-leaflet'
+import 'leaflet/dist/leaflet.css'
+import DataInputDialog from '../components/DataInputDialog.vue'
+import { getOutlineForLatLng, getRandomPastelColor } from '../js/helpers.js'
+import { getPolygonAndName } from '../js/helpers-new.js'
+import * as requests from '../js/requests.js'
+import { getUser, login, logout } from '../js/user'
 
 export default {
   components: {
@@ -74,10 +71,10 @@ export default {
   },
   computed: {
     isLoggedIn() {
-      return getUser() !== null;
+      return getUser() !== null
     },
     username() {
-      return getUser().username;
+      return getUser().username
     }
   },
   methods: {
@@ -105,19 +102,22 @@ export default {
      * @param {string} data.budget
      */
     async saveData(data) {
-      console.log("saveData", data)
+      console.log('saveData', data)
 
       const { lat, lng } = this.selectedCoords
-      const saveResponse = await requests.createPin({
-        name: data.name,
-        description: data.description,
-        date: data.date,
-        companions: data.companions,
-        duration: data.duration,
-        budget: data.budget,
-        latitude: lat,
-        longitude: lng
-      }, this.token)
+      const saveResponse = await requests.createPin(
+        {
+          name: data.name,
+          description: data.description,
+          date: data.date,
+          companions: data.companions,
+          duration: data.duration,
+          budget: data.budget,
+          latitude: lat,
+          longitude: lng
+        },
+        this.token
+      )
 
       // add the marker
       this.markers.push({
@@ -126,21 +126,21 @@ export default {
       })
 
       // add also the polygon (returned by the post request)
-      console.log("saveResponse", saveResponse)
-      let polygonLatlngs = saveResponse.polygon;
+      console.log('saveResponse', saveResponse)
+      let polygonLatlngs = saveResponse.polygon
       this.polygons.push({
         key: this.polygons.length + 1,
         latlngs: polygonLatlngs,
         color: getRandomPastelColor(),
         polygonname: saveResponse.polygonname
       })
-      this.clickedOnMap = false;
+      this.clickedOnMap = false
     },
     /**
      * Cancels the DataInputDialog
      */
     cancelData() {
-      this.clickedOnMap = false;
+      this.clickedOnMap = false
     },
     async updatePinsAndPolygons() {
       this.markers = []
@@ -155,11 +155,11 @@ export default {
         })
       })
 
-      console.log("pins", pins)
+      console.log('pins', pins)
 
       // create the polygons for the pins
       for (let i = 0; i < pins.length; i++) {
-        const pin = pins[i];
+        const pin = pins[i]
         const polygonLatlngs = pin.polygon
         this.polygons.push({
           key: this.polygons.length + 1,
