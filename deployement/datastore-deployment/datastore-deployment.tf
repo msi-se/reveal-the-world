784ed919e383828e18818ab7abb9a6e4ae2055bf
Anais <anais.calza@gmail.com>
2024-01-17 14:48:22 +0100

    terraform deployment finished + yaml fix

--- deployement/datastore-deployment/datastore-deployment.tf
+++ deployement/datastore-deployment/datastore-deployment.tf
@@ -80,4 +80,5 @@ resource "azurerm_postgresql_server" "postgresql" {
 
   public_network_access_enabled    = false
   ssl_enforcement_enabled          = false
+  ssl_minimal_tls_version_enforced = "TLSEnforcementDisabled"
 }
