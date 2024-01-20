0e9d73e6731d48cca6e4d46a31111890818b186a
Johannes Brandenburger <79154528+johannesbrandenburger@users.noreply.github.com>
2024-01-20 16:05:29 +0100

    new welcome screen

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,20 +1,21 @@
 <template>
   <v-layout>
     <v-app-bar :elevation="16" :color="tenantBackgroundColor">
-      <img
-        alt="logo-black"
-        :src="TENANT_LOGO"
-        width="200"
-        class="d-inline-block align-top ml-2"
-        style="cursor: pointer"
-      />
+      <a :href="`${PATH}/`" class="d-inline-block ml-2 align-middle">
+        <img
+          alt="logo-white"
+          :src="TENANT_LOGO"
+          width="200"
+          style="cursor: pointer"
+        />
+      </a>
       <a :href="`${PATH}/`" style="text-decoration: none; color: inherit">
         <div class="mr-15 ml-4 app-bar-title" style="cursor: pointer">
           {{ TENANT === 'default' ? '' : TENANT }}
         </div>
       </a>
       <v-spacer></v-spacer>
-      <a :href="`${PATH}/pin`">
+      <a :href="`${PATH}/pin`" v-if="isLoggedIn">
         <v-btn class="mr-2"> My Pins </v-btn>
       </a>
       <a :href="`${PATH}/heatmap`">
